30319a03e4c37cc53e732fa3b2771dba7c5f07d7
Hector Sanchez <c_hsanchez@groupon.com>
2023-01-06 23:31:30 -0300

    adding desafio 05

--- desafios/desafio01.py
+++ desafios/desafio01.py
@@ -4,6 +4,7 @@ import numpy.linalg as l
 def exercise0():
     # Horizontal vectors
     # creating vector 1
+    # array makes arrays of 1-D or n-D
     print("\n*** exercise 0 ***")
     v1 = np.array([1,1,1])
     # creating vector 2
@@ -12,7 +13,7 @@ def exercise0():
     print("\nPrinting V1: ",v1,"Printing V2: ",v2)
 
     # vertical vectors
-    # Using reshape attribute to change the dimension of the array
+    # Using reshape attribute to change the dimension of the array (row,column)
     vector1 = v1.reshape(3,1)
     vector2 = v2.reshape(3,1)
     print("vertical vectors")
@@ -29,6 +30,12 @@ def exercise0():
 
 def exercise1(matrix):
     # Using the Linear Algebra module for numpy linalg
+
+    print("\n*** exercise 1 ***")
+
+    #Using norm method from Linear Algebra module
+    #axis=1 will allow us to get the norm by rows from the matrix
+    #which means we are taking the norm from each vector of the matrix
 
     # l-infinity
     matrix_infinity_norm = l.norm(matrix, np.inf, axis=1)
@@ -52,47 +59,27 @@ def exercise2(norms, matrix):
 
     # sorting norm l2
     array_norms = np.array(norms)
-    array_norms[::-1].sort() #mutating the array for decreasing
-    print("\nsorting numbers: ",array_norms)
+    array_norms[::-1].sort() #mutating the array for decreasing. instead of creating a new array
+    print("sorting numbers: ",array_norms)
 
-    #Identify position
-    Va = array_norms[0]
-    Vb = array_norms[1]
-    Vc = array_norms[2]
+    #checking the size of the array
+    size = array_norms.size
+    print("size of the array: ", size)
 
-    print("Va: ", Va)
-    print("Vb: ", Vb)
-    print("Vc: ", Vc)
-
-    #from the original matrix
-    V1 = matrix[0]
-    V2 = matrix[1]
-    V3 = matrix[2]
-
-    print("original vectors v1: ",V1,"v2: ",V2,"v3: ",V3)
-    #Reorder matrix
+    #Reordering the new matrix
     new_matrix = []
+    values = {}
+    i = 0
+    for i in range(size):
+        val = str("x"+str(i)) 
+        values[val] = array_norms[i]
 
-    if Va == l.norm(V1, 2):
-        new_matrix.append(V1)
-    elif Va == l.norm(V2, 2):
-        new_matrix.append(V2)
-    else:
-        new_matrix.append(V3)
-    
-    if Vb == l.norm(V1, 2):
-        new_matrix.append(V1)
-    elif Vb == l.norm(V2, 2):
-        new_matrix.append(V2)
-    else:
-        new_matrix.append(V3)
-    
-    if Vc == l.norm(V1, 2):
-        new_matrix.append(V1)
-    elif Vc == l.norm(V2, 2):
-        new_matrix.append(V2)
-    else:
-        new_matrix.append(V3)
+        if values[val] == l.norm(matrix[i], 2):
+            new_matrix.append(matrix[i])
+        elif values[val] == l.norm(matrix[i-1], 2):
+            new_matrix.append(matrix[i-1])
+        else:
+            new_matrix.append(matrix[i-2])
     
     # creating the original matrix
     new_matrix = np.array(new_matrix)
@@ -158,8 +145,40 @@ def exercise4(matrix):
     print("\neigenvalues: ",eigenvalues)
     print("\neigenvector: \n",eigenvectors)
 
+    return
+
 def exercise5():
-    pass
+    
+    print("\n*** exercise 5 ***")
+
+    #vectors
+    x1 = np.array([1,2,3])
+    x2 = np.array([4,5,6])
+    x3 = np.array([7,8,9])
+    X = np.array([x1,x2,x3])
+
+    print("X Vectors: x1: ",x1,"x2: ",x2,"x3: ",x3)
+
+    c1 = np.array([1,0,0])
+    c2 = np.array([0,1,1])
+    C = np.array([c1,c2])
+
+    print("C Vectors: c1: ",c1,"c2: ",c2)
+
+    #calculating the distance between the X vectors and C vectors.
+    dist01 = l.norm(x1 - c1)
+    print ("distance between x1 and c1: ", dist01)
+    dist02 = l.norm(x1 - c2)
+    print ("distance between x1 and c2: ", dist02)
+    dist03 = l.norm(x2 - c1)
+    print ("distance between x2 and c1: ", dist03)
+    dist04 = l.norm(x2 - c2)
+    print ("distance between x1 and c2: ", dist04)
+    dist05 = l.norm(x3 - c1)
+    print ("distance between x1 and c1: ", dist05)
+    dist06 = l.norm(x3 - c2)
+    print ("distance between x2 and c2: ", dist06)
+
 
 def main():
     #exercise0
